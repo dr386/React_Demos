378e13048ad87d6b8e5906f6a1d99d90c7d01eca
dr386 <dr386.student@gmail.com>
2021-08-16 21:25:09 -0700

    no message

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,23 +1,25 @@
 import React from "react";
-import axios from "axios";
+import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
+import ImageList from "./ImageList";
 
 class App extends React.Component {
-  async onSearchSubmit(term) {
-    const response = await axios
-      .get("https://api.unsplash.com/search/photos", {
-        params: { query: term },
-        headers: {
-          Authorization:
-            "Client-ID KfR-SnH0yP3VlzqkXhNSs7Jot_C3UxpquvQCbOLEXvQ",
-        },
-      });
-  }
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
+    // unsplash baseURL: "https://api.unsplash.com", headers: {Authorization: API_key}
+    const response = await unsplash.get("/search/photos", {
+      params: { query: term },
+    });
+
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        <ImageList images={this.state.images} />
       </div>
     );
   }
---
+++ pics/src/components/ImageList.js
@@ -0,0 +1,11 @@
+import './ImageList.css';
+import React from 'react';
+
+const ImageList = (props) => {
+  const images = props.images.map(({ alt_description, id, urls }) => {
+    return <img alt={alt_description} key={id} src={urls.regular} />;
+  });
+  return <div className="image-list">{images}</div>;
+};
+
+export default ImageList;
