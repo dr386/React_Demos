6f517f078f46f1b0d0aae1599aab9a936d95d7ee
dr386 <dr386.student@gmail.com>
2021-08-31 13:05:52 -0700

    181

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Search from "./components/Search";
-// import Accordion from "./components/Accordion";
+import Accordion from "./components/Accordion";
+import Dropdown from "./components/Dropdown";
+
+const options = [
+  {
+    label: "The color red",
+    value: "red",
+  },
+  {
+    label: "The color green",
+    value: "green",
+  },
+  {
+    label: "The color blue",
+    value: "blue",
+  },
+];
 
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
-      <Search />
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
   );
 };
